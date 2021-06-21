6d7f79ff13535792566503e044771ac08369c745
Dan Gray Jr <85451793+dangrayjr@users.noreply.github.com>
2021-06-21 02:53:46 +0000

    Started to add Questions and Answers for part 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -9,28 +9,36 @@ let candidateName = ``;
 let question = "Who was the first American woman in space?";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions;
+let questions = [
+  'True or false: 5 kilometer == 5000 meters? ',
+  '(5 + 3)/2 * 10 = ?	',
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  'What is the minimum crew size for the ISS?	'];
 let correctAnswers;
 let candidateAnswers;
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName = input.question(`Please enter your name: `);
+  candidateName = input.question(`Please enter your name: `);
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   console.log(question);
-candidateAnswer = input.question(`Answer: `);
-
-
+  candidateAnswer = input.question(`Answer: `);
+  console.log(questions);
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+ if (candidateAnswer == correctAnswer){
+    console.log('Correct!');
+  }
+  else {
+    console.log('Incorrect.');
+  }
 
   //let grade;
 
@@ -42,7 +50,7 @@ function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
   //  console.log(`Thank you, ${candidateName} taking our quiz.`)
-  
+
   askQuestion();
   //gradeQuiz(this.candidateAnswers);
 }
